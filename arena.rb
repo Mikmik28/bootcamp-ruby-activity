2d38a4710b17b5cdf97daec08aa8075c01e33bd1
Mikmik28 <mikeeagregado28@gmail.com>
2023-03-27 14:58:25 +0800

    Renamed health to hp

--- arena.rb
+++ arena.rb
@@ -10,16 +10,16 @@ class Arena
   def start
     puts "Welcome to the Arena!"
     puts "You are fighting #{enemy.name}!"
-    while player.health > 0 && enemy.health > 0
+    while player.hp > 0 && enemy.hp > 0
       # 4. Implement turns. Player and Enemy will do a random action.
       # ACTIONS are: attack and use_potion
       # enter code here
 
-      puts "Your health: #{player.health}"
-      puts "#{enemy.name}'s health: #{enemy.health}"
+      puts "Your health: #{player.hp}"
+      puts "#{enemy.name}'s health: #{enemy.hp}"
     end
 
-    if player.health > 0
+    if player.hp > 0
       puts "You win!"
     else
       puts "You lose!"
